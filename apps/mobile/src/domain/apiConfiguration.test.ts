c1b0630ee47b4e595822337bddf77db1fd47edc6
Waaberi Ibrahim <wibra007@uottawa.ca>
2026-07-18 13:30:34 -0400

    Use neutral API URL fixtures

--- apps/mobile/src/domain/apiConfiguration.test.ts
+++ apps/mobile/src/domain/apiConfiguration.test.ts
@@ -4,13 +4,13 @@ import test from 'node:test';
 import { resolveApiUrl } from './apiConfiguration';
 
 test('uses the launcher-provided API URL by default', () => {
-  assert.equal(resolveApiUrl('http://100.117.203.24:8000', ''), 'http://100.117.203.24:8000');
+  assert.equal(resolveApiUrl('http://developer-tailnet-host.test:8000', ''), 'http://developer-tailnet-host.test:8000');
 });
 
 test('does not let a stale saved URL override launcher configuration', () => {
   assert.equal(
-    resolveApiUrl('http://100.117.203.24:8000', ' http://10.0.2.2:8000 '),
-    'http://100.117.203.24:8000',
+    resolveApiUrl('http://developer-tailnet-host.test:8000', ' http://10.0.2.2:8000 '),
+    'http://developer-tailnet-host.test:8000',
   );
 });
 
